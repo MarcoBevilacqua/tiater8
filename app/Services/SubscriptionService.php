16b8ef628de9d1610ff1d78695b717618782d682
mbevilacqua <Ch@n@ndl3rB0ng>
2021-06-19 20:48:08 +0200

    removed unused import + added method

--- app/Services/SubscriptionService.php
+++ app/Services/SubscriptionService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Subscription;
+use Carbon\Carbon;
 
 class SubscriptionService {
 
@@ -11,6 +12,18 @@ class SubscriptionService {
         return Subscription::where('subscription_email', '=', $email)
         ->where('status', '=', 0)
         ->count() > 0;
+    }
+
+    public static function subscriptionCanBeConfirmed(string $token) {
+        $subscriptionByToken = Subscription::where('token', $token)
+        ->first();
+
+        if(!$subscriptionByToken) {
+            return false;
+        }
+
+        return $subscriptionByToken->expires_at > Carbon::now() &&
+            $subscriptionByToken->status === Subscription::TO_BE_COMPLETED;
     }
 
 }
