56fe0d5a46136f450b3307128260d8e21a161e72
mbevilacqua <Ch@n@ndl3rB0ng>
2021-06-19 20:51:02 +0200

    updated test

--- tests/Feature/SubscriptionTest.php
+++ tests/Feature/SubscriptionTest.php
@@ -6,6 +6,9 @@ use App\Models\Subscription;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+use PhpParser\Node\NullableType;
 use Tests\TestCase;
 
 class SubscriptionTest extends TestCase
@@ -49,14 +52,14 @@ class SubscriptionTest extends TestCase
     /**
      * @test
      *
-     * 
+     *
      * @return void
      */
     public function shouldUpdateSubscriptionWhenPageIsReached()
     {
         $response = $this->post('/subscriptions/init', ['customer_email' => 'abc123@gmail.com']);
-        $this->assertDatabaseHas('subscriptions', ['expires_at' => NULL]);
-        //assert response        
+        $this->assertDatabaseHas('subscriptions', ['expires_at' => null]);
+        //assert response
         $getFormResponse = $this->get($response->getContent());
         $getFormResponse->assertStatus(200);
         //assert subscription has changed
@@ -64,5 +67,39 @@ class SubscriptionTest extends TestCase
             'expires_at' => Carbon::now()->addMinutes(10),
             'status' => 1
             ]);
+    }
+
+    /**
+     * @test
+     *
+     *
+     * @return void
+     */
+    public function subscriptionSubmitShouldUpdateStatus()
+    {
+        $subscriptionToBeCompleted = Subscription::factory()->toBeCompleted()->create();
+
+        //subscription has to be confirmed, user should fill the form
+        $this->assertDatabaseHas('subscriptions', ['status' => 1]);
+        //user fills the form and hit "submit"
+        $subscriptionData = [
+            'first_name' => 'Marco',
+            'last_name' => 'Bevilacqua',
+            'customer_email' => 'example@mail.com',
+            'sub_token' => $subscriptionToBeCompleted->token
+        ];
+
+        $response = $this->post('subscriptions/complete', $subscriptionData);
+
+        $this->assertDatabaseHas('customers', [
+            'email' => 'example@mail.com',
+            'first_name' => 'Marco',
+            'last_name' => 'Bevilacqua',
+            'password' => NULL
+        ]);
+
+        $this->assertDatabaseHas('subscriptions', [
+            'status' => Subscription::getStatusId(Subscription::TO_BE_CONFIRMED)
+        ]);
     }
 }
