16c36aeacf4a25ecfec9bcf975bdb7172ee7c84e
mbevilacqua <Ch@n@ndl3rB0ng>
2021-11-17 00:43:31 +0100

    Pairing with old subscription controller methods

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# TIATER
+
+v0.0.1
+
+### Gestionale per piccole compagnie teatrali e associazioni culturali
+
+```
+Tech stack
+
+-   PHP 7.4
+-   Laravel 7
+-   Mysql
+-   VueJs
+-   Inertia
+```
---
+++ app/Http/Controllers/PublicSubscriptionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PublicSubscriptionController extends Controller
+{
+    /**
+     * PAIRINGS WITH SUBSCRIPTION CONTROLLER METHODS
+     *
+     * INDEX --> GENERATE
+     * STORE --> INIT
+     * EDIT --> FILL
+     * UPDATE --> COMPLETE
+     */
+}
