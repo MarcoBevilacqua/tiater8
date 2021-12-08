da99241dbba785d04744cbcb88f79242c797cf6a
mbevilacqua <Ch@n@ndl3rB0ng>
2021-12-08 22:31:41 +0100

    build

--- app/Http/Controllers/MailTestController.php
+++ app/Http/Controllers/MailTestController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SubscriptionFilled;
+use App\Mail\SubscriptionToComplete;
+use Illuminate\Support\Facades\URL;
 
 class MailTestController extends Controller
 {
     public function send()
     {
-        Mail::to('dontstop.marco@gmail.com')
-        ->send(new SubscriptionFilled());
+        $randomString = substr(str_shuffle(MD5(microtime())), 0, 22);
+
+        Mail::to('juliamanocchio@gmail.com')
+            ->send(
+                new SubscriptionToComplete(
+                URL::signedRoute('subscriptions.fill', [
+                'token' => $randomString])
+            )
+            );
+
+        return "OK";
     }
 }
