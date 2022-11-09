7b44e9f039c765f82f246f302de3a6b14932c878
Marco <dontstop.marco@gmail.com>
2022-11-09 23:50:20 +0100

    improved customers autocomplete + minor fix

--- resources/js/Components/Bookings/Modal.vue
+++ resources/js/Components/Bookings/Modal.vue
@@ -89,7 +89,7 @@
                             <form @submit.prevent="delete">
                                 <input v-model="form.customer_id" name="customer_id" type="hidden">
                                 <button
-                                    class="w-full md:w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-5 py-2 text-red-600 font-medium hover:underline sm:w-1/2 sm:text-sm"
+                                    class="w-full md:w-full inline-flex justify-center rounded-md border border-transparent px-5 py-2 text-red-600 font-medium hover:underline sm:w-1/2 sm:text-sm"
                                     type="submit"
                                 >
                                     Cancella
@@ -127,11 +127,41 @@
                                     </div>
                                 </div>
                                 <div class="my-4"></div>
+
                                 <button
+                                    v-if="!form.processing"
                                     class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:w-full sm:text-sm"
                                     type="submit"
                                 >
-                                    Aggiorna
+                                    Assegna
+                                </button>
+                                <button
+                                    v-else
+                                    type="button"
+                                    class="group relative w-full flex justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-400 hover:bg-indigo-600 transition ease-in-out duration-150 cursor-not-allowed"
+                                    disabled=""
+                                >
+                                    <svg
+                                        class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <circle
+                                            class="opacity-25"
+                                            cx="12"
+                                            cy="12"
+                                            r="10"
+                                            stroke="currentColor"
+                                            stroke-width="4"
+                                        ></circle>
+                                        <path
+                                            class="opacity-75"
+                                            fill="currentColor"
+                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                        ></path>
+                                    </svg>
+                                    Operazione in corso...
                                 </button>
                             </form>
                         </div>
@@ -212,16 +242,18 @@ export default {
     watch: {
         search: {
             handler: throttle(function () {
-                axios.get(
-                    '/api/customers', {
-                        params:
-                            {term: this.search}
-                    }
-                ).then(res => {
-                    this.suggestions = res.data
-                }).catch(err => {
-                    console.log(err)
-                })
+                if(this.search.length > 3){
+                    axios.get(
+                        '/api/customers', {
+                            params:
+                                {term: this.search}
+                        }
+                    ).then(res => {
+                        this.suggestions = res.data
+                    }).catch(err => {
+                        console.log(err)
+                    })
+                }
             }, 150),
             deep: true
         }
