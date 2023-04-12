1c708bd00fc8ba35c60858164e820ae84a719161
Marco <dontstop.marco@gmail.com>
2023-04-12 18:23:57 +0200

    minor fix

--- resources/js/Pages/Public/SelfInvitation.vue
+++ resources/js/Pages/Public/SelfInvitation.vue
@@ -4,11 +4,11 @@
             <div class="flex justify-items-center">
                 <div class="w-full">
                     <img
-                        class="mx-auto rounded-md"
                         :src="'/img/common/logo.jpg'"
                         alt="pci_logo"
-                        width="120"
+                        class="mx-auto rounded-md"
                         height="80"
+                        width="120"
                     />
                 </div>
             </div>
@@ -20,7 +20,7 @@
                         Ciao, inserisci la tua mail!
                     </h2>
                     <small
-                        >Sarai reindirizzato al form di completamento
+                    >Sarai reindirizzato al form di completamento
                         dati</small
                     >
                 </div>
@@ -28,26 +28,27 @@
                     class="col-span-3 mx-8 md:col-start-2 md:col-span-1 md:mx-2"
                 >
                     <form class="mt-4 space-y-6" @submit.prevent="submit">
-                        <input type="hidden" name="remember" value="true" />
+                        <input name="remember" type="hidden" value="true"/>
                         <div class="rounded-md shadow-sm -space-y-px">
                             <div>
-                                <label for="customer_email" class="sr-only"
-                                    >Email address</label
+                                <label class="sr-only" for="customer_email"
+                                >Email address</label
                                 >
                                 <input
-                                    v-model="form.customer_email"
                                     id="customer_email"
-                                    name="customer_email"
+                                    v-model="form.customer_email"
+                                    autofocus
+                                    class="bg-white appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm text-center"
+                                    name="customer_email" placeholder="Il tuo indirizzo mail"
+                                    required
                                     type="email"
-                                    class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm text-center"
-                                    placeholder="Il tuo indirizzo mail"
                                 />
                             </div>
                         </div>
                         <div v-if="!form.processing">
                             <button
-                                type="submit"
                                 class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                type="submit"
                             >
                                 <span
                                     class="absolute left-0 inset-y-0 flex items-center pl-3"
@@ -58,15 +59,15 @@
                         </div>
                         <div v-else>
                             <button
-                                type="button"
                                 class="group relative w-full flex justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-400 hover:bg-indigo-600 transition ease-in-out duration-150 cursor-not-allowed"
                                 disabled=""
+                                type="button"
                             >
                                 <svg
                                     class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                                    xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
                                     viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
                                 >
                                     <circle
                                         class="opacity-25"
@@ -78,8 +79,8 @@
                                     ></circle>
                                     <path
                                         class="opacity-75"
-                                        fill="currentColor"
                                         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                        fill="currentColor"
                                     ></path>
                                 </svg>
                                 Operazione in corso...
@@ -106,8 +107,7 @@
 
 <script>
 import Container from "@/Layouts/Container";
-import { useForm } from "@inertiajs/inertia-vue3";
-import { Inertia } from "@inertiajs/inertia";
+import {useForm} from "@inertiajs/inertia-vue3";
 
 export default {
     components: {
@@ -131,7 +131,7 @@ export default {
             });
         }
 
-        return { form, submit };
+        return {form, submit};
     },
 
     props: {
