2ab7b29557ad17f5527fddcc944423abe016f14c
Marco <dontstop.marco@gmail.com>
2023-04-12 17:34:06 +0200

    dusk env file + test

--- tests/Browser/PublicSubscriptionTest.php
+++ tests/Browser/PublicSubscriptionTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Browser;
 
+use App\Models\Subscription;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class PublicSubscriptionTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
     /**
      * A Dusk test example.
      *
@@ -15,9 +18,19 @@ class PublicSubscriptionTest extends DuskTestCase
      */
     public function testExample()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+
+        $sub = Subscription::factory()->toBeConfirmed()->create();
+
+        $this->assertDatabaseHas('subscriptions', ['subscription_email' => $sub->subscription_email]);
+
+        $this->browse(function (Browser $browser) use ($sub) {
+            $browser->visit('/over/subscriptions/start')
+                ->assertInputPresent('customer_email')
+                ->type('customer_email', $sub->subscription_email)
+                ->press('Conferma')
+                ->waitForText('Indirizzo email non disponibile', 8)
+                ->screenshot('sub');
+
         });
     }
 }
