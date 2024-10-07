ff62ac056176c8d2a5276274aaa2b48d7decb4e9
Marco <dontstop.marco@gmail.com>
2024-10-07 17:39:56 +0200

    fix on middleware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
-class TrustProxies
+class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
