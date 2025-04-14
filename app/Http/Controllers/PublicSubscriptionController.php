49cd44b8d6880a3ad80e4d798cdd1715c90c440a
Marco <dontstop.marco@gmail.com>
2025-04-14 19:07:27 +0200

    import fix

--- app/Http/Controllers/PublicSubscriptionController.php
+++ app/Http/Controllers/PublicSubscriptionController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdatePublicSubscriptionRequest;
 use App\Mail\SubscriptionFilled;
-use App\Mail\SubscriptionToComplete;
 use App\Models\Customer;
 use App\Models\Subscription;
+use App\Services\MailService;
+use App\Services\PublicSubscriptionService;
 use App\Services\SubscriptionService;
-use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cookie;
@@ -74,8 +75,13 @@ class PublicSubscriptionController extends Controller
             'customer_email' => 'required|email:filter'
         ]);
 
-        //check if sub should be renewed
+        // create token
+        $token = substr(str_shuffle(MD5(microtime())), 0, 22);
+
+        // If sub should be renewed redirect to renew form
         $shouldRenew = SubscriptionService::subscriptionShouldBeRenewed($request->get('customer_email'));
+
+        // redirect
         if($shouldRenew) {
             Log::info("Renewing subscription for {$request->get('customer_email')}...");
             return Redirect::to(URL::signedRoute('subscriptions.renew', [
@@ -84,50 +90,25 @@ class PublicSubscriptionController extends Controller
             Log::info("New Subscription incoming...");
         }
 
-        //the url to be returned
-        $randomString = substr(str_shuffle(MD5(microtime())), 0, 22);
-
-        //retrieve year_from and year_to
-        $years = SubscriptionService::getSubscriptionYears();
-
         //create a to-be-confirmed subscription
         try {
-            Subscription::create([
-                'subscription_email' => $request->customer_email,
-                'status' => Subscription::PENDING,
-                'token' => $randomString,
-                'customer_id' => null,
-                'year_from' => $years['from'],
-                'year_to' => $years['to'],
-            ]);
+            PublicSubscriptionService::createToBeConfirmed($request->customer_email, $token);
         } catch (\Exception $exception) {
-            Log::error("Cannot create pending subscription with email {$request->customer_email} and token {$randomString}: {$exception->getMessage()}");
+            Log::error("Cannot create pending subscription with email {$request->customer_email} and token {$token}: {$exception->getMessage()}");
             return Redirect::back()->with('error', "Indirizzo email non disponibile");
         }
 
-        Log::info("Pending Subscription for email {$request->customer_email} has been created!", [__CLASS__, __FUNCTION__]);
-
-        //if request is coming from the admin area, mail should be sent as invitation.
-        //otherwise, just make the redirect to the subscription.fill route
+        /**
+         * if request is coming from the admin area, mail should be sent as invitation.
+         * otherwise, just make the redirect to the subscription.fill route
+         */
         if (!$request->is('over/*')) {
-            try {
-                Mail::to($request->customer_email)
-                    ->send(new SubscriptionToComplete(URL::signedRoute('subscriptions.fill', [
-                        'token' => $randomString])));
-            } catch (\Exception $exception) {
-                Log::error("Cannot send Mail to {$request->customer_email}: " . $exception->getMessage());
-            }
-            if (Mail::failures()) {
-                Log::error("Cannot send email!!!");
-            } else {
-                Log::info("Mail to {$request->customer_email} has been sent! Redirecting...", [__CLASS__, __FUNCTION__]);
-            }
+            // send email notification
+            MailService::sendToCompleteSubscription($request->customer_email, $token);
+            // return to internal subscription
             return Redirect::route('subscriptions.index');
         }
-
-
-        return Redirect::to(URL::signedRoute('subscriptions.fill', [
-            'token' => $randomString]));
+        return Redirect::to(URL::signedRoute('subscriptions.fill', ['token' => $token]));
     }
 
     /**
@@ -136,22 +117,16 @@ class PublicSubscriptionController extends Controller
      * @param string $token
      * @return Response
      */
-    public function edit(string $token)
+    public function edit(string $token): Response
     {
         try {
-            $subscriptionToFill = Subscription::where('token', '=', $token)
-                ->where('status', Subscription::PENDING)
-                ->firstOrFail();
-        } catch (\Exception $modelNotFoundException) {
-            Log::error("Cannot Find pending Subscription to fill: " . $modelNotFoundException->getMessage());
-            abort(401);
+            PublicSubscriptionService::updatePendingSubscription($token);
+        } catch (\Exception $ex) {
+            Log::error("Cannot update pending Subscription: " . $ex->getMessage());
+            return \response()->abort(\Symfony\Component\HttpFoundation\Response::HTTP_UNAUTHORIZED);
         }
 
-        $subscriptionToFill->update([
-            'expires_at' => Carbon::now()->addMinutes(10),
-        ]);
-
-        Log::info("Subscription can be completed!!!", [__CLASS__, __FUNCTION__]);
+        Log::info("Subscription can be completed, redirecting...", [__CLASS__, __FUNCTION__]);
 
         //should return form
         return Inertia::render('Public/CompleteSubscription', [
@@ -166,10 +141,10 @@ class PublicSubscriptionController extends Controller
     /**
      * Completes the subscription and creates customer entry
      *
-     * @param Request $request
+     * @param UpdatePublicSubscriptionRequest $request
      * @return RedirectResponse
      */
-    public function update(Request $request): RedirectResponse
+    public function update(UpdatePublicSubscriptionRequest $request): RedirectResponse
     {
         /**
          * 1. check if subscription is valid
@@ -178,29 +153,13 @@ class PublicSubscriptionController extends Controller
          * 4. return the response
          */
 
-        Log::info("Completing subscription...");
-        $request->validate([
-            'first_name' => 'required',
-            'last_name' => 'required',
-            'address' => 'required',
-            'city' => 'required',
-            'phone' => 'required',
-            'birth' => 'required',
-            'resident' => 'required',
-            'fiscal_code' => 'required|size:16'
-        ]);
-
         Log::info("Request validated, proceeding...");
 
-
-        //TODO: check if subscription is valid
-        if (!$request->has('sub_token') || !$request->input('sub_token')) {
-            Log::error("Cannot retrieve Token from request, aborting");
-            abort(400);
-        }
+        // get validated data
+        $validated = $request->validated();
 
         //checking if data is correct
-        $canHandleSubscription = SubscriptionService::subscriptionCanBeConfirmed($request->input('sub_token'));
+        $canHandleSubscription = SubscriptionService::subscriptionCanBeConfirmed($validated['sub_token']);
 
         if (!$canHandleSubscription->count()) {
             Log::error("Cannot find subscription with token " . $request->input('sub_token'));
@@ -209,18 +168,9 @@ class PublicSubscriptionController extends Controller
 
         //Create the customer
         try {
-            $customer = Customer::create([
-                'first_name' => $request->input('first_name'),
-                'last_name' => $request->input('last_name'),
-                'city' => $request->input('city'),
+            $customer = Customer::create(
+                $validated + [
                 'email' => $canHandleSubscription->subscription_email, //should not be handled from the public form
-                'phone' => $request->input('phone'),
-                'birth' => $request->input('birth'),
-                'province' => $request->input('province'),
-                'resident' => $request->input('resident'),
-                'address' => $request->input('address'),
-                'postal_code' => $request->input('postal_code'),
-                'fiscal_code' => $request->input('fiscal_code'),
             ]);
         } catch (\Exception $ex) {
             Log::error("Cannot create customer with data " . implode(",", $request->all()) . ": Error: {$ex->getMessage()}");
@@ -228,6 +178,7 @@ class PublicSubscriptionController extends Controller
         }
 
         Log::info("Customer with ID {$customer->id} successfully created", [__CLASS__, __FUNCTION__]);
+        
         //create cookie
         $cookie = Cookie::make('subscription-confirmed', true, 1);
         try {
